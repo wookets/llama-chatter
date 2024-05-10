6bad6beafa95d587777ab50a2ce8b4d742951504
Sean Wesenberg <wookets@gmail.com>
2024-05-10 13:26:52 -0500

    chore: more code reuse

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 
-import requests
-import json
-import sys
 import utils
 
 # read in all the endpoints we are going to be chatting with
@@ -13,31 +10,8 @@ main_llm = "llama3"
 main_prompt = "Give me a response I can use to start a discussion with other LLMs."
 
 # pick a main llm in which we will ask "Lets start a discussion with other LLMs"
-response = requests.post(llms.get(main_llm), json= {
-  "model": main_llm,
-  "prompt": main_prompt,
-})
-# Check if the response was successful
-if response.status_code != 200:
-  print("Error:", response.status_code, response.text)
-  sys.exit(1)  # Exit the program with an error code
-
-final_message = ""
-total_duration = 0
-load_duration = 0
-# Iterate through each line in the response content
-for line in response.iter_lines():
-  if line:  # check if line is not empty
-    json_object = utils.dotdict(json.loads(line.decode('utf-8')))  # decode and parse the JSON
-    # Now you can work with json_object as a Python dictionary
-    print(json_object)
-    if json_object.done:
-      total_duration = json_object.total_duration / 1000000 / 1000
-      load_duration = json_object.load_duration / 1000000 / 1000
-    else:
-      final_message += json_object.response
-print(f"Response: {final_message}")
-print(f"Took {total_duration} seconds.")
+response = utils.send_message(llms.get(main_llm), main_llm, main_prompt)
+print(response)
 
 # go around the horn and ask each LLM the same question and get responses back
 for llm, url in llms.items():
--- utils.py
+++ utils.py
@@ -1,5 +1,36 @@
+
+import json
+import sys
+import requests
+
 class dotdict(dict):
   """dot.notation access to dictionary attributes"""
   __getattr__ = dict.get
   __setattr__ = dict.__setitem__
   __delattr__ = dict.__delitem__
+
+def send_message(url, model, prompt):
+  response = requests.post(url, json= {
+    "model": model,
+    "prompt": prompt,
+  })
+  if response.status_code != 200:
+    print("Error:", response.status_code, response.text)
+    sys.exit(1)
+
+  # Iterate through each line in the response content
+  final_message = ""
+  total_duration = 0
+  load_duration = 0
+  for line in response.iter_lines():
+    if line:  # check if line is not empty
+      json_object = dotdict(json.loads(line.decode('utf-8')))  # decode and parse the JSON
+      #print(json_object)
+      if json_object.done:
+        total_duration = json_object.total_duration / 1000000 / 1000
+        load_duration = json_object.load_duration / 1000000 / 1000
+      else:
+        final_message += json_object.response
+  #print(f"Response: {final_message}")
+  #print(f"Took {total_duration} seconds.")
+  return final_message
